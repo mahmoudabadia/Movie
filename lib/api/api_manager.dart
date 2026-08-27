1e4fdb3f75cbe38c34b4c9eeefff3136341d4330
Sama kh Nasr <samanasrallah2004@gmail.com>
2026-08-27 14:43:16 +0300

    refactor: migrate networking layer to Dio for Home Screen

--- lib/api/api_manager.dart
+++ lib/api/api_manager.dart
@@ -1,47 +1,40 @@
-import 'dart:convert';
-import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+import 'package:movie_app/api/constants/end_point.dart';
 import 'constants/api_constant.dart';
 import 'model/available_movies_response.dart';
-import 'constants/end_point.dart';
 
 class ApiManager {
+  static final Dio _dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://${ApiConstants.baseUrl}/',
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+    ),
+  );
+
+
   static Future<AvailableMoviesResponse?> getMovies({
     String? genre,
     int limit = 20,
     String sortBy = 'date_added',
     String orderBy = 'desc',
   }) async {
-    Uri url = Uri.https(
-      ApiConstants.baseUrl,
-      ApiEndpoints.listMovies,
-      {
-        'sort_by': sortBy,
-        'order_by': orderBy,
-        'limit': limit.toString(),
-
-        if (genre != null && genre.isNotEmpty) 'genre': genre.toLowerCase(),
-      },
-    );
-
     try {
-      var response = await http.get(url);
-      if (response.statusCode == 200) {
-        var json = jsonDecode(response.body);
-        return AvailableMoviesResponse.fromJson(json);
-      } else {
-        if (kDebugMode) {
-          print("Server Error: ${response.statusCode}");
-        }
-        return null;
+      var response = await _dio.get(
+        ApiEndpoints.listMovies,
+        queryParameters: {
+          'limit': limit,
+          'sort_by': sortBy,
+          'order_by': orderBy,
+          if (genre != null && genre.isNotEmpty) 'genre': genre.toLowerCase(),
+        },
+      );
+
+      if (response.statusCode == 200 && response.data != null) {
+        return AvailableMoviesResponse.fromJson(response.data);
       }
-    } catch (e, stackTrace) {
-      if (kDebugMode) {
-        print("================ API ERROR ================");
-        print(e);
-        print(stackTrace);
-        print("===========================================");
-      }
+      return null;
+    } catch (e) {
       return null;
     }
   }
