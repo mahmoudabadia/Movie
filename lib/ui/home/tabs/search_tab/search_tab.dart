8aad752c2f2d91b62871ad8e904ed3afc4d9daef
DESKTOP-M44KT1F\user <3brother.a.m.a@gmail.com>
2026-09-03 02:44:36 +0300

    search_tab_edit

--- lib/ui/home/tabs/search_tab/search_tab.dart
+++ lib/ui/home/tabs/search_tab/search_tab.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/api/api_manager.dart';
-import 'package:movie_app/api/model/available_movies_response.dart';
 import 'package:movie_app/api/model/movies.dart';
+import 'package:movie_app/utils/app_text_styles.dart';
+import 'package:movie_app/utils/size_utils.dart';
+
+import '../../../../l10n/app_localizations.dart';
+import '../../../../utils/app_assets.dart';
+import '../../../../utils/app_colors.dart';
+import '../../../widgets/custom_text_field.dart';
+import '../home_tab/movie_details/movie_details_widget.dart';
 
 class SearchTab extends StatefulWidget {
   const SearchTab({super.key});
@@ -58,74 +65,105 @@ class _SearchTabState extends State<SearchTab> {
 
   @override
   Widget build(BuildContext context) {
+    var height = context.height;
+    var width = context.width;
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            const SizedBox(height: 12),
-            TextField(
+            CustomTextField(
               controller: _searchController,
               onChanged: _searchMovies,
-              style: const TextStyle(color: Colors.white, fontSize: 16),
-              cursorColor: Colors.white,
-              decoration: InputDecoration(
-                hintText: 'Search for a movie...',
-                hintStyle: const TextStyle(color: Colors.grey),
-                prefixIcon: const Icon(Icons.search, color: Colors.white),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
+              textStyle: AppTextStyles.regular16White,
+              hintText: AppLocalizations.of(context)!.search,
+              fillColor: AppColors.grayColor,
+              prefixIcon: Image.asset(AppAssets.iconSearch),
+              borderColor: AppColors.transparent,
+              hintStyle: AppTextStyles.regular16White,
             ),
-            const SizedBox(height: 16),
+
+            SizedBox(height: height * 0.02),
             Expanded(
               child: _isLoading
                   ? const Center(child: CircularProgressIndicator())
                   : GridView.builder(
-                itemCount: _movies.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 12,
-                  mainAxisSpacing: 12,
-                  childAspectRatio: 0.65,
-                ),
-                itemBuilder: (context, index) {
-                  var movie = _movies[index];
-                  return ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: Stack(
-                      fit: StackFit.expand,
-                      children: [
-                        movie.mediumCoverImage != null
-                            ? Image.network(movie.mediumCoverImage!, fit: BoxFit.cover)
-                            : Container(color: Colors.grey, child: const Icon(Icons.movie)),
-                        Positioned(
-                          top: 8,
-                          left: 8,
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
-                            decoration: BoxDecoration(
-                              color: Colors.black.withOpacity(0.7),
-                              borderRadius: BorderRadius.circular(6),
-                            ),
-                            child: Row(
-                              children: [
-                                Text(
-                                  movie.rating?.toString() ?? '0.0',
-                                  style: const TextStyle(color: Colors.white, fontSize: 12),
-                                ),
-                                const SizedBox(width: 4),
-                                const Icon(Icons.star, color: Colors.amber, size: 14),
-                              ],
-                            ),
+                      itemCount: _movies.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 12,
+                            mainAxisSpacing: 12,
+                            childAspectRatio: 0.65,
                           ),
-                        ),
-                      ],
+                      itemBuilder: (context, index) {
+                        var movie = _movies[index];
+                        return ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Stack(
+                            fit: StackFit.expand,
+                            children: [
+                              movie.mediumCoverImage != null
+                                  ? InkWell(
+                                      onTap: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                MovieDetailsWidget(
+                                                  id: movie.id,
+                                                ),
+                                          ),
+                                        );
+                                      },
+                                      child: Image.network(
+                                        movie.mediumCoverImage!,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    )
+                                  : Container(
+                                      color: AppColors.grayColor,
+                                      child: const Icon(Icons.movie),
+                                    ),
+                              Positioned(
+                                top: 8,
+                                left: 8,
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 6,
+                                    vertical: 2,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    color: AppColors.blackColor.withValues(
+                                      alpha: 0.7,
+                                    ),
+                                    borderRadius: BorderRadius.circular(6),
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        movie.rating?.toString() ?? '0.0',
+                                        style: const TextStyle(
+                                          color: AppColors.whiteColor,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      const SizedBox(width: 4),
+                                      Image.asset(
+                                        AppAssets.iconStar,
+                                        height: height * .02,
+                                        width: width * .03,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
             ),
           ],
         ),
